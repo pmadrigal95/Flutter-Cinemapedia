f2bc2805e5d3b9d5900e3426a0dda408cfb4153e
Pablo Madrigal <pablomadrigal.1995@gmail.com>
2023-08-08 20:45:12 -0600

    Environment Variables y Git

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,15 +1,19 @@
+
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'package:cinemapedia/config/constants/enviroment.dart';
 
+class HomeScreen extends StatelessWidget {
   static const name = 'home_screen';
 
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Placeholder(),
+    return  Scaffold(
+      body: Center(
+        child: Text(Environment.theMovieDbKey),
+      ),
     );
   }
 }
